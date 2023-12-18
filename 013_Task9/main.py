01129a69d0a5f3ad21a4ba4db2504d14fab2c140
Andrey Golubev <laissezfaireisfair@gmail.com>
2023-12-18 13:26:35 +0700

    Finish lab 9.1

--- 013_Task9/main.py
+++ 013_Task9/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 def load_and_scale(categorical_columns: [str], file_name: str, target_column: str) \
@@ -21,7 +25,39 @@ def load_and_scale(categorical_columns: [str], file_name: str, target_column: st
     features_test_scaled = pd.DataFrame(scaler.transform(features_test))
     target_train_scaled = pd.DataFrame(scaler.fit_transform(target_train))
     target_test_scaled = pd.DataFrame(scaler.transform(target_test))
+
     return features_train_scaled, features_test_scaled, target_train_scaled, target_test_scaled
+
+
+def test_decision_tree_accuracy(features_test_scaled, features_train_scaled, target_test_scaled, target_train_scaled):
+    model = DecisionTreeClassifier()
+    model.fit(features_train_scaled, target_train_scaled)
+
+    prediction = model.predict(features_test_scaled)
+
+    accuracy = accuracy_score(target_test_scaled, prediction)
+    return accuracy
+
+
+def test_xgb_classifier_accuracy(features_test_scaled, features_train_scaled, target_test_scaled, target_train_scaled):
+    model = XGBClassifier()
+    model.fit(features_train_scaled, target_train_scaled)
+
+    prediction = model.predict(features_test_scaled)
+
+    accuracy = accuracy_score(target_test_scaled, prediction)
+    return accuracy
+
+
+def test_logistic_regression_accuracy(features_test_scaled, features_train_scaled, target_test_scaled,
+                                      target_train_scaled):
+    model = LogisticRegression()
+    model.fit(features_train_scaled, target_train_scaled)
+
+    prediction = model.predict(features_test_scaled)
+
+    accuracy = accuracy_score(target_test_scaled, prediction)
+    return accuracy
 
 
 def main():
@@ -32,11 +68,24 @@ def main():
     features_train_scaled, features_test_scaled, target_train_scaled, target_test_scaled \
         = load_and_scale(categorical_columns, file_name, target_column)
 
-    print(pd.concat((features_test_scaled, target_test_scaled), axis=1))
+    label_encoder = LabelEncoder()
+    target_train_scaled = label_encoder.fit_transform(target_train_scaled)
+    target_test_scaled = label_encoder.fit_transform(target_test_scaled)
 
-    # TODO: Learn models and check accuracy
+    decision_tree_accuracy = test_decision_tree_accuracy(features_test_scaled, features_train_scaled,
+                                                         target_test_scaled, target_train_scaled)
 
-    pass
+    print(f'Decision tree accuracy: {decision_tree_accuracy}')
+
+    xgb_accuracy = test_xgb_classifier_accuracy(features_test_scaled, features_train_scaled,
+                                                target_test_scaled, target_train_scaled)
+
+    print(f'XGBoost accuracy: {xgb_accuracy}')
+
+    logistic_regression_accuracy = test_logistic_regression_accuracy(features_test_scaled, features_train_scaled,
+                                                                     target_test_scaled, target_train_scaled)
+
+    print(f'Logistic regression accuracy: {logistic_regression_accuracy}')
 
 
 main()
